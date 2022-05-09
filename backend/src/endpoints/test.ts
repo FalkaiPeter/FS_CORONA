b2840bfbd21a9d47ebd58776d1a5c8312fd1323b
FalkaiPeter <formal.pfalkai@gmail.com>
2022-05-09 23:17:45 +0200

    test endpoint added

--- backend/src/endpoints/test.ts
+++ backend/src/endpoints/test.ts
@@ -3,8 +3,9 @@ import { model } from "mongoose";
 
 const handler: RequestHandler = async (req, res) => {
   const newestRecord = await model("corona").findOne().sort({ _id: -1 });
-  console.log(newestRecord);
-  res.status(200).send(newestRecord);
+  console.log(process.env.DATABASE_URI);
+  console.log("test");
+  res.status(200).send(process.env.DATABASE_URI);
 };
 
 export const refreshRoute = (app: Application) => app.get("/test", handler);
